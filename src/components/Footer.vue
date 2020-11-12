592d35911e20536277da6dbef730e381751e564b
clf94-dev <clf94@hotmail.es>
2020-11-12 10:10:52 +0100

    deleted unnecessary widths footer component

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -55,5 +55,139 @@ export default {
 </script>
 
 <style scoped>
+.footer-section {
+    background-color: #012d32;
+    padding: 5% 7%;
+}
 
+@media screen and (max-width: 756px) {
+    .footer-section {
+        text-align: center;
+    }
+}
+
+.footer-section .logo-col img {
+    height: 30px;
+    margin-bottom: 15px;
+}
+
+.footer-section .logo-col .limks-row .link-col {
+    margin-bottom: 20px;
+}
+
+.footer-section .logo-col .limks-row .link-col .links {
+    text-align: center;
+    cursor: pointer !important;
+    color: white !important;
+}
+
+.footer-section .logo-col .limks-row .link-col .links:hover {
+    transition: all 0.3s ease;
+    color: #f67e7e !important;
+}
+
+@media screen and (max-width: 756px) {
+    .footer-section .logo-col .limks-row .link-col .links.home {
+        position: relative;
+        left: 45px;
+    }
+}
+
+@media screen and (max-width: 756px) {
+    .footer-section .logo-col .limks-row .link-col .links.about {
+        position: relative;
+        left: -45px;
+    }
+}
+
+.footer-section .dir-col {
+    width: 100%;
+    color: white;
+    font-size: 15px;
+    font-weight: 300;
+    margin-bottom: 25px;
+}
+
+@media screen and (max-width: 1080px) {
+    .footer-section .dir-col {
+        text-align: end;
+    }
+}
+
+@media screen and (max-width: 756px) {
+    .footer-section .dir-col {
+        text-align: center;
+    }
+}
+
+.footer-section .social-hidden-col .hidden {
+    display: none;
+}
+
+@media screen and (min-width: 756px) and (max-width: 1080px) {
+    .footer-section .social-hidden-col .hidden {
+        display: block;
+        color: white;
+    }
+
+    .footer-section .social-hidden-col .hidden i {
+        cursor: pointer;
+        margin-right: 10px;
+    }
+
+    .footer-section .social-hidden-col .hidden i:hover {
+        transition: all 0.3s ease;
+        color: #f67e7e;
+    }
+}
+
+@media screen and (min-width: 756px) and (max-width: 1080px) {
+    .footer-section .social-col .copy {
+        text-align: end;
+        margin-left: 50%;
+    }
+}
+
+.footer-section .social-col .copy p {
+    font-size: 12px;
+    color: white;
+    font-family: "Livvic", sans-serif;
+    font-weight: 300;
+}
+
+.footer-section .social-col .social-row {
+    width: 100%;
+    margin-bottom: 10px;
+    display: block;
+    color: white;
+    margin-top: 10px;
+}
+
+@media screen and (min-width: 800px) {
+    .footer-section .social-col .social-row {
+        width: 100%;
+    }
+}
+
+@media screen and (min-width: 1080px) {
+    .footer-section .social-col .social-row {
+        margin-bottom: 40px;
+    }
+}
+
+@media screen and (min-width: 756px) and (max-width: 1080px) {
+    .footer-section .social-col .social-row {
+        display: none !important;
+    }
+}
+
+.footer-section .social-col .social-row i {
+    cursor: pointer;
+    margin-right: 10px;
+}
+
+.footer-section .social-col .social-row i:hover {
+    transition: all 0.3s ease;
+    color: #f67e7e;
+}
 </style>
