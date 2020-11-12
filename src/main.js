988fd89762c31194147560ae119d96876256db74
clf94-dev <clf94@hotmail.es>
2020-11-12 08:03:51 +0100

    add init config font awesome, router & vuelidate

--- src/main.js
+++ src/main.js
@@ -1,7 +1,19 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
 import Vue from 'vue'
+import './plugins/bootstrap-vue'
 import App from './App.vue'
 
+import '@fortawesome/fontawesome-free/css/all.css'     
+import '@fortawesome/fontawesome-free/js/all.js'  
+
+import VueRouter from 'vue-router'
+
+import Vuelidate from 'vuelidate';
+
 Vue.config.productionTip = false
+Vue.use(VueRouter);
+Vue.use(Vuelidate);
 
 new Vue({
   render: h => h(App),
