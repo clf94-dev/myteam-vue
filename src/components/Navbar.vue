1347d15e31cc95b57df2bcfcd2220d5f75e4afbc
clf94-dev <clf94@hotmail.es>
2020-11-12 09:10:18 +0100

    added navbar struct and image assets

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,12 +1,35 @@
 <template>
-<div>
-    <h1>Navbar component</h1>
-</div>
+<nav class='navbar'>
+    <div class="navbar-cont">
+        <div class='navbar-logo'>
+            <img :src="logo" alt="myteam" />
+        </div>
+        <div class="menu-icon">
+            <i class='fas fa-bars'></i>
+        </div>
+        <ul class='nav-menu'>
+            <li class="nav-item">
+                <router-link class="nav-links" to='/home'> Home</router-link>
+
+            </li>
+            <li class="nav-item">
+                <router-link class="nav-links" to='/about'> About</router-link>
+            </li>
+            <button class='transparent-btn'> Contact Us</button>
+        </ul>
+
+    </div>
+</nav>
 </template>
 
 <script>
 export default {
-    name: 'Navbar'
+    name: 'Navbar',
+    data() {
+        return {
+            logo: require('@/assets/images/logo.svg')
+        }
+    }
 }
 </script>
 
