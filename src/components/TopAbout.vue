e7e09019b99e98e5a25e26b3665c740f97d6b292
clf94-dev <clf94@hotmail.es>
2020-11-12 09:26:46 +0100

    added topAbout struct component

--- src/components/TopAbout.vue
+++ src/components/TopAbout.vue
@@ -1,7 +1,16 @@
 <template>
-<div>
-    <h1>top about component</h1>
-</div>
+<b-container fluid class="topAbout-section">
+    <b-row>
+        <b-col class="title-col" xs="12" sm="12" md="6" lg="6">
+            <h2>About</h2>
+        </b-col>
+        <b-col class="text-col" xs="12" sm="12" md="6" lg="6">
+            <hr />
+            <p>We help companies build dynamic teams made up of top global talent. Using our network of passionate professionals we drive innovation and deliver incredible outcomes. Talented, diverse teams shape the best products and experiences. We’ll bring
+                those teams to you.</p>
+        </b-col>
+    </b-row>
+</b-container>
 </template>
 
 <script>
