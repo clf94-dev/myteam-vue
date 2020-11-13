fd13e7a027b73a541c1dd54edb7acbd3a7032fb5
clf94-dev <clf94@hotmail.es>
2020-11-13 08:47:28 +0100

    added struct cta component

--- src/components/Cta.vue
+++ src/components/Cta.vue
@@ -1,12 +1,24 @@
 <template>
-<div>
-    <h1>cta component</h1>
-</div>
+<b-container fluid class="cta-section">
+    <b-row>
+        <b-col class="title-col" xs="12" sm="12" md="6" lg="6">
+            <h2>Ready to get started?</h2>
+        </b-col>
+        <b-col class="btn-col" xs="12" sm="12" md="6" lg="6">
+            <button class='transparent-btn' @click='goToContact()'>Contact Us</button>
+        </b-col>
+    </b-row>
+</b-container>
 </template>
 
 <script>
 export default {
-    name: 'Cta'
+    name: 'Cta',
+    methods: {
+        goToContact() {
+            this.$router.push('/contact');
+        }
+    }
 }
 </script>
 
