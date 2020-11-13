aad85ca378c15cc63ec81b10dc457fe52b872517
clf94-dev <clf94@hotmail.es>
2020-11-13 10:19:28 +0100

    added struct MeetCard component

--- src/components/Meet.vue
+++ src/components/Meet.vue
@@ -2,8 +2,8 @@
 <b-container fluid class="meet-section">
     <h3>Meet the directors</h3>
     <b-row class="meet-row">
-        <b-col xs="12" sm="6" md="4" lg="4" v-for="data of meet" v-bind:key="data.name">
-            <MeetCard :name="data.name" :position="data.position" :description="data.description" :src="data.src" :ImgAlt="data.imgAlt"></MeetCard>
+        <b-col class="card-col" xs="12" sm="6" md="4" lg="4" v-for="data of meet" v-bind:key="data.name">
+            <MeetCard :name="data.name" :position="data.position" :description="data.description" :src="data.src" :imgalt="data.imgAlt"></MeetCard>
         </b-col>
     </b-row>
 </b-container>
@@ -28,39 +28,34 @@ export default {
                 {
                     name: "Cristian Duncan",
                     position: "Co-founder & COO",
-                    description: "“Distributed teams required unique processes. You need to approach work in a new" +
-                        " way.”   ",
+                    description: "“Distributed teams required unique processes. You need to approach work in a new way.”   ",
                     src: require('@/assets/images/avatar-christian.jpg'),
                     imgAlt: "Cristian Duncan"
                 },
                 {
                     name: "Cruz Hamer",
                     position: "Co-founder & CTO",
-                    description: "“Technology is at the forefront of enabling distributed teams.That is where we c" +
-                        "ome in.” ",
+                    description: "“Technology is at the forefront of enabling distributed teams.That is where we come in.” ",
                     src: require('@/assets/images/avatar-cruz.jpg'),
                     imgAlt: "Cruz Hamer"
                 }, {
                     name: "Drake Heaton",
                     position: "Business Development Lead",
-                    description: "“Hiring similar people from similar backgrounds is a surefire way to stunt innov" +
-                        "ation.” ",
+                    description: "“Hiring similar people from similar backgrounds is a surefire way to stunt innovation.” ",
                     src: require('@/assets/images/avatar-drake.jpg'),
                     imgAlt: "Drake Heaton"
                 },
                 {
                     name: "Griffin Wise",
                     position: "Lead Marketing",
-                    description: "“Unique perspectives shape unique products, which is what you need to survive th" +
-                        "ese days.” ",
+                    description: "“Unique perspectives shape unique products, which is what you need to survive these days.” ",
                     src: require('@/assets/images/avatar-griffin.jpg'),
                     imgAlt: "Griffin Wise"
                 },
                 {
                     name: "Aden Allan",
                     position: "Head of Talent",
-                    description: "“Empowered teams create truly amazing products. Set the north star and let them " +
-                        "follow it.” ",
+                    description: "“Empowered teams create truly amazing products. Set the north star and let them follow it.” ",
                     src: require('@/assets/images/avatar-aden.jpg'),
                     imgAlt: "Aden Allan"
                 }
--- src/components/MeetCard.vue
+++ src/components/MeetCard.vue
@@ -1,12 +1,67 @@
 <template>
 <div>
-    <h1>meetCard component</h1>
+    <div :class="card1Classes">
+        <img class='avatar' :src="src" :alt="ImgAlt" />
+
+        <p class='name'>{{name}}
+        </p>
+
+        <p class='position'>{{position}}
+        </p>
+    </div>
+    <div :class="card2Classes">
+        <p class='name'>{{name}}
+        </p>
+        <p class='description'>{{description}}</p>
+        <div class="social-row" fxLayout='row wrap'>
+            <i class='fab fa-2x fa-twitter'></i>
+            <i class='fab fa-2x fa-linkedin'></i>
+        </div>
+
+    </div>
+    <button @click="changeState()" :class="classes">
+        <img :src="imgSrc" :alt="imgAlt">
+
+    </button>
 </div>
 </template>
 
 <script>
 export default {
-    name: 'MeetCard'
+    name: 'MeetCard',
+    props: [
+        'name', 'position', 'description', 'imgalt', 'src'
+    ],
+    data() {
+        return {
+            Plus: require('@/assets/images/icon-cross.svg'),
+            Cross: require('@/assets/images/icon-close.svg'),
+            click: false,
+            imgSrc: this.Plus,
+            imgAlt: "More",
+            classes: "btn coral",
+            card1Classes: "info-card1 show",
+            card2Classes: "info-card2  hide",
+        }
+    },
+    methods: {
+        changeState() {
+            this.click = !this.click;
+            if (!this.click) {
+                this.imgSrc = this.Plus;
+                this.imgAlt = "More";
+                this.classes = "btn coral";
+                this.card1Classes = "show";
+                this.card2Classes = "hide";
+            } else {
+                this.imgSrc = this.Cross;
+                this.imgAlt = "Close";
+                this.classes = " btn light-blue";
+                this.card1Classes = "hide";
+                this.card2Classes = "show";
+            }
+        }
+    }
 }
 </script>
 
