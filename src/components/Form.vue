9e56deadcc794c166546626fe36ca4dffd2c63f7
clf94-dev <clf94@hotmail.es>
2020-11-15 07:52:46 +0100

    added form component struct

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,7 +1,13 @@
 <template>
-<div>
-    <h1>form component</h1>
-</div>
+<form class="form-col">
+    <input type="text" placeholder="Name">
+    <input type="text" placeholder="Email">
+    <input type="text" placeholder="Company">
+    <input type="text" placeholder="Title">
+    <textarea placeholder="Your message..." name="message" id="" cols="30" rows="10"></textarea>
+
+    <button type="submit" class="submit-btn"> Submit </button>
+</form>
 </template>
 
 <script>
