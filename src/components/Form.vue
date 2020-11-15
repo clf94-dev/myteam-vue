81f466cb13642d42bbfcdd1c5e225677e5ee192f
clf94-dev <clf94@hotmail.es>
2020-11-15 07:55:15 +0100

    added style to form component

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -17,5 +17,55 @@ export default {
 </script>
 
 <style scoped>
+input {
+    width: 100%;
+    outline: none;
+    border: none;
+    font-family: "Livvic", sans-serif;
+    color: white;
+    border-bottom: 1px solid #2c6168;
+    background-color: transparent;
+    height: 50px;
+    margin-bottom: 15px;
+}
 
+input.error::placeholder {
+    color: #f67e7e;
+}
+
+textarea {
+    width: 100%;
+    height: 150px;
+    outline: none;
+    border: none;
+    font-family: "Livvic", sans-serif;
+    color: white;
+    border-bottom: 1px solid #2c6168;
+    background-color: transparent;
+    margin-bottom: 35px;
+}
+
+textarea.error::placeholder {
+    color: #f67e7e;
+}
+
+.submit-btn {
+    margin-top: 3%;
+    margin-bottom: 10%;
+    color: white;
+    border: 1px solid #012d32;
+    background-color: #012d32;
+    border-radius: 25px;
+    height: 50px;
+    width: 200px;
+    font-family: "Livvic", sans-serif;
+}
+
+.submit-btn:hover {
+    cursor: pointer;
+    transition: all 0.2s ease;
+    background-color: white;
+    color: #014d56;
+    border: 1px solid white;
+}
 </style>
