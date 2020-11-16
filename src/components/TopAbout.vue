41701e5381dfe15ecf634f4fefd63db44716a744
clf94-dev <clf94@hotmail.es>
2020-11-16 07:40:23 +0100

    added AOS to topAbout component

--- src/components/TopAbout.vue
+++ src/components/TopAbout.vue
@@ -1,10 +1,10 @@
 <template>
 <b-container fluid class="topAbout-section">
     <b-row>
-        <b-col class="title-col" xs="12" sm="12" md="6" lg="6">
+        <b-col class="title-col" xs="12" sm="12" md="6" lg="6" data-aos='fade-right'>
             <h2>About</h2>
         </b-col>
-        <b-col class="text-col" xs="12" sm="12" md="6" lg="6">
+        <b-col class="text-col" xs="12" sm="12" md="6" lg="6" data-aos='fade-left' data-aos-delay='300'>
             <hr />
             <p>We help companies build dynamic teams made up of top global talent. Using our network of passionate professionals we drive innovation and deliver incredible outcomes. Talented, diverse teams shape the best products and experiences. We’ll bring
                 those teams to you.</p>
