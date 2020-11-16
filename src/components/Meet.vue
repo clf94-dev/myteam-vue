2c4e1215b8c65b75c4c69031689c622250e9a769
clf94-dev <clf94@hotmail.es>
2020-11-16 07:45:17 +0100

    added AOS to meet component

--- src/components/Meet.vue
+++ src/components/Meet.vue
@@ -2,7 +2,7 @@
 <b-container fluid class="meet-section">
     <h3>Meet the directors</h3>
     <b-row class="meet-row">
-        <b-col class="card-col" xs="12" sm="6" md="4" lg="4" v-for="data of meet" v-bind:key="data.name">
+        <b-col class="card-col" xs="12" sm="6" md="4" lg="4" v-for="data of meet" v-bind:key="data.name" data-aos="flip-left" :data-aos-delay="data.delay">
             <MeetCard :info="data"></MeetCard>
         </b-col>
     </b-row>
@@ -23,41 +23,47 @@ export default {
                     position: "Founder & CEO",
                     description: "“It always amazes me how much talent there is in every corner of the globe.” ",
                     src: require('@/assets/images/avatar-nikita.jpg'),
-                    imgAlt: "Nikita Marks"
+                    imgAlt: "Nikita Marks",
+                    delay: 200
                 },
                 {
                     name: "Cristian Duncan",
                     position: "Co-founder & COO",
                     description: "“Distributed teams required unique processes. You need to approach work in a new way.”   ",
                     src: require('@/assets/images/avatar-christian.jpg'),
-                    imgAlt: "Cristian Duncan"
+                    imgAlt: "Cristian Duncan",
+                    delay: 400
                 },
                 {
                     name: "Cruz Hamer",
                     position: "Co-founder & CTO",
                     description: "“Technology is at the forefront of enabling distributed teams.That is where we come in.” ",
                     src: require('@/assets/images/avatar-cruz.jpg'),
-                    imgAlt: "Cruz Hamer"
+                    imgAlt: "Cruz Hamer",
+                    delay: 600
                 }, {
                     name: "Drake Heaton",
                     position: "Business Development Lead",
                     description: "“Hiring similar people from similar backgrounds is a surefire way to stunt innovation.” ",
                     src: require('@/assets/images/avatar-drake.jpg'),
-                    imgAlt: "Drake Heaton"
+                    imgAlt: "Drake Heaton",
+                    delay: 300
                 },
                 {
                     name: "Griffin Wise",
                     position: "Lead Marketing",
                     description: "“Unique perspectives shape unique products, which is what you need to survive these days.” ",
                     src: require('@/assets/images/avatar-griffin.jpg'),
-                    imgAlt: "Griffin Wise"
+                    imgAlt: "Griffin Wise",
+                    delay: 500
                 },
                 {
                     name: "Aden Allan",
                     position: "Head of Talent",
                     description: "“Empowered teams create truly amazing products. Set the north star and let them follow it.” ",
                     src: require('@/assets/images/avatar-aden.jpg'),
-                    imgAlt: "Aden Allan"
+                    imgAlt: "Aden Allan",
+                    delay: 700
                 }
             ]
         }
