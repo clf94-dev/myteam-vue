72a46cc93b1ade13a4aed740c052eb6e3ffcd226
clf94-dev <clf94@hotmail.es>
2020-11-16 07:31:56 +0100

    added AOS FeaturesHome component

--- src/components/FeaturesHome.vue
+++ src/components/FeaturesHome.vue
@@ -1,16 +1,16 @@
 <template>
 <b-container fluid class="features-section">
     <b-row>
-        <b-col class="title-col" xs="12" sm="6" md="6" lg="6">
+        <b-col data-aos='fade-right' data-aos-delay='700' class="title-col" xs="12" sm="6" md="6" lg="6">
             <hr />
             <h2>Build & manage distributed teams like no one else.</h2>
         </b-col>
         <b-col class="feat-col" xs="12" sm="6" md="6" lg="6">
             <b-row class="feat-elem" v-for="data of featuresHome" v-bind:key="data.title">
-                <b-col class="img-col" xs="2" sm="2" md="2" lg="2">
+                <b-col class="img-col" xs="2" sm="2" md="2" lg="2" data-aos='fade-up' :data-aos-delay="data.delay">
                     <img :src="data.src" :alt="data.imgAlt">
                 </b-col>
-                <b-col class="text-col" xs="10" sm="10" md="10" lg="10">
+                <b-col class="text-col" xs="10" sm="10" md="10" lg="10" data-aos='fade-up' :data-aos-delay="data.delay">
                     <h3>{{data.title}}</h3>
                     <p>{{data.text}}</p>
                 </b-col>
@@ -32,19 +32,22 @@ export default {
                     title: "Experienced Individuals",
                     text: "Our network is made up of highly experienced professionals who are passionate about what they do.",
                     src: require('@/assets/images/icon-person.svg'),
-                    imgAlt: "Experienced Individuals"
+                    imgAlt: "Experienced Individuals",
+                    delay: 900
                 },
                 {
                     title: "Easy to Implement",
                     text: "Our processes have been refined over years of implementation meaning our teams always deliver.",
                     src: require('@/assets/images/icon-cog.svg'),
-                    imgAlt: " Easy to Implement"
+                    imgAlt: " Easy to Implement",
+                    delay: 1100
                 },
                 {
                     title: "Enhanced Productivity",
                     text: "Our customized platform with in-built analytics helps you manage your distributed teams.",
                     src: require('@/assets/images/icon-chart.svg'),
-                    imgAlt: "Enhanced Productivity"
+                    imgAlt: "Enhanced Productivity",
+                    delay: 1300
                 }
             ]
         }
@@ -84,7 +87,7 @@ export default {
     font-family: "Livvic", sans-serif;
     color: white;
     font-weight: 300;
-    padding-right: 35%;
+    padding-right: 40%;
 }
 
 @media screen and (max-width: 755px) {
@@ -134,6 +137,7 @@ export default {
     color: #f67e7e;
     font-weight: 400;
     margin-bottom: 7px;
+
 }
 
 .features-section .feat-col .feat-elem .text-col p {
